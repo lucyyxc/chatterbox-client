0e38d5bd3464ef7bddceb0f291708cb726dc94eb
sband1 <sbandeali1@gmail.com>
2020-10-17 12:09:33 -0700

    Allow user to post messages

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -1,6 +1,7 @@
 var FormView = {
 
   $form: $('form'),
+  $text: $('#message'),
 
   initialize: function() {
     FormView.$form.on('submit', FormView.handleSubmit);
@@ -8,9 +9,32 @@ var FormView = {
 
   handleSubmit: function(event) {
     // Stop the browser from submitting the form
+    // console.log('EVENT', event);
     event.preventDefault();
-    
+    // console.log('prevent default', event.preventDefault());
     console.log('click!');
+    //use .val() on the id message
+    var $texts = FormView.$text.val();
+    //create message obj
+    var message = {
+      username: App.username,
+      text: $texts,
+      roomname: 'lobby'
+    };
+    var $texts = FormView.$text.val('');
+    //create successCB
+    var successCB = function() {
+      console.log('chatterbox: Message submitted');
+      App.fetch();
+      //App.initialize();
+      //MessagesView.initialize();
+    };
+    //create errorCB
+    var errorCB = function() {
+      console.log('chatterbox: Failed to submit message', data);
+    };
+    //call Parse.create
+    Parse.create(message, successCB, errorCB);
   },
 
   setStatus: function(active) {
