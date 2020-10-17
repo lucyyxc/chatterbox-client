ce45b012218981818c3a0d45efe0ff38d779c21a
sband1 <sbandeali1@gmail.com>
2020-10-17 12:06:17 -0700

    Refactor roomView to not accepet undefined room names

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -4,8 +4,12 @@ var RoomsView = {
   $select: $('#rooms select'),
 
   initialize: function() {
+    console.log('hi');
     var successCB = function(data) {
       for (var i = 0; i < data.results.length; i++) {
+        if (data.results[i].roomname === undefined) {
+          continue;
+        }
         RoomsView.renderRoom(data.results[i].roomname);
       }
     };
@@ -17,12 +21,11 @@ var RoomsView = {
 
   renderRoom: function(roomname) {
     //use underscore template on each room property
-    // "<b>&lt;script&gt;</b>"
     //create conditional to assess if roomname already exists in drop down list
     var compile = _.template(`
       <option value="<%- roomname %>"><%- roomname %></option>
     `);
-    var $roomname = $('<option value=roomname><roomname></option>');
+    var $roomname = $(`<option value="${roomname}">${roomname}</option>`);
     RoomsView.$select.append($roomname);
 
   }
