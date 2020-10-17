d8e44570de1841b5c10aff360c63a28727f5a3d2
sband1 <sbandeali1@gmail.com>
2020-10-16 21:26:03 -0700

    Implement Parse.create function

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -4,6 +4,17 @@ var Parse = {
 
   create: function(message, successCB, errorCB = null) {
     // todo: save a message to the server
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+      url: Parse.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function(error) {
+        console.error('chatterbox: Failed to fetch messages', error);
+      }
+    });
   },
 
   readAll: function(successCB, errorCB = null) {
