95abc519f6c606d9a03004f22eaef01dad012184
sband1 <sbandeali1@gmail.com>
2020-10-16 21:27:26 -0700

    Implement messageView and messgaesView methods

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,14 @@
 var MessageView = {
 
   render: _.template(`
-      <!--
-      <div class="chat">
-        <div class="username"></div>
-        <div></div>
-      </div>
-      -->
-    `)
-
+    <div class="chat">
+      <div class="username"><%- username %></div>
+      <div class="text"><%- text %></div>
+      <div class="roomname"><%- roomname %></div>
+    </div>
+  `)
+//https://underscorejs.org/#template
+//MessageView.render(messgeObj)
+// place object properties into html elements
+// returns a string with updated html element
 };
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,20 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    var successCB = function(data) {
+      for (var i = 0; i < data.length; i++) {
+        MessagesView.renderMessage(data[i]);
+      }
+    };
+    var errorCB = function(data) {
+      console.error('chatterbox: Failed to send message', data);
+    };
+    Parse.readAll(successCB, errorCB);
   },
 
-  render: function() {
+  renderMessage: function (message) {
+    var compiled = MessageView.render(message);
+    MessagesView.$chats.append(compiled);
   }
 
 };
